ce1a997b46b1fe3274e878ad2632ff3bd6b87d02
Kirill <dr.mokrousoff2017yandex.ru>
2025-07-26 16:57:41 +0300

    Добавления Loading screen

--- Assets/![Jam]Summer/Scripts/Root.cs
+++ Assets/![Jam]Summer/Scripts/Root.cs
@@ -1,15 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using BitterCMS.UnityIntegration;
 using BitterCMS.UnityIntegration.Utility;
+using UnityEngine;
 
 public class Root : RootMonoBehavior
 {
+    [SerializeField]
+    private UIRoot _uIRoot;
+
     public GridMaster Grid;
     public CardList CardList;
     public PlayerMaster Player;
     public EnemyMaster Enemy;
 
     protected override void GlobalStart()
+    {
+        _uIRoot.CanvasRoot.gameObject.SetActive(false);
+
+
+        CoroutineUtility.Run(LoadGame());
+    }
+    private IEnumerator LoadGame()
     {
         Player = GetComponent<PlayerMaster>();
         Enemy = GetComponent<EnemyMaster>();
@@ -18,8 +30,13 @@ public class Root : RootMonoBehavior
         Player.Init();
         Enemy.Init();
 
+        yield return _uIRoot.HudRoot.RadialDissolveController.StartDissolveAnimation(4f);
+        _uIRoot.CanvasRoot.gameObject.SetActive(true);
+
         CoroutineUtility.Run(Loop());
+        yield break;
     }
+
     private IEnumerator Loop()
     {
         yield return Player.Step();
---
+++ Assets/![Jam]Summer/Scripts/UI/HUD.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    [field: SerializeField]
+    public RadialDissolveController RadialDissolveController { get; private set; }
+}
+
